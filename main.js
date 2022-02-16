efe48aa2c5fef7e62796f4caaa79519305f6236c
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-02-16 01:26:02 -0800

    add new format for join section

--- main.js
+++ main.js
@@ -182,9 +182,9 @@ const swiper1 = new Swiper('.swiper1', {
 
 
 //hide loader when window loads and add 2 seconds to loading time
-window.addEventListener('load',setTimeout(() => {
+window.addEventListener('load',() =>setTimeout(() => {
   document.querySelector('.loader-content').classList.add('hide');
-}, 2000)
+}, 1500)
 );
 
 
@@ -197,3 +197,33 @@ window.addEventListener('load',setTimeout(() => {
 //   block: 'center',
 //   inline: 'center',
 // })
+
+
+
+
+
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    
+    const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+      currentlyActiveAccordionItemHeader.classList.toggle("active");
+      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
